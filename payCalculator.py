e844e8cca5f570ebb222e46042c7726b5d94f91e
Keegan Ehler <Keegan.Ehler@vu.com>
2023-02-02 15:50:53 -0600

    Asked name, Hours, Pay rate. Multiplied Pay rate by hours and printed as an Integer.

--- payCalculator.py
+++ payCalculator.py
@@ -1,8 +1,22 @@
 def calculatePay():
     # Implement your solution in between the two comment blocks
     print("calculating pay")
-    # This first line is provided for you
-    hrs = input("Enter Hours:")
+   
+   #Greeting
+name = input("What is your name? ")
+print(f'Hello, {name}!')
+
+#Request the pay rate
+rate = input('What is your hourly pay rate? ')
+
+#Request number of hours worked
+hours = input ('How many hours did you work? ')
+
+#Calculate total pay
+pay = int(rate)*int(hours)
+
+#Display total payout
+print(f'Your total pay will be ${pay}!')
 
     # end assignment
 
